879e8125731b47cf4691e8a2e1817acdc2fd7859
Shaqlin Mondal <79474217+ShaqlinMondal-creater@users.noreply.github.com>
2023-05-06 15:17:45 +0530

    Add files via upload

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.css'
+import { Outlet, Link, NavLink } from "react-router-dom"
+
+function Home() {
+  const img = "./assets/pizza-with-turkey-bacon-orange-and-cashew-nuts-2022-03-29-07-33-54-utc.jpg";
+  return (
+    <React.Fragment>
+      <section className="home" id="home" style={{ backgroundImage: `url(${img})` }}>
+        <div className="content">
+        <h3>Fast Food Delivery</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt perferendis obcaecati iste voluptatum, quaerat nihil magnam numquam sint? </p>
+        <a href="#" className="btn">order now</a>
+        </div>
+    </section>
+
+    {/* <>
+    <Outlet />
+    </> */}
+    
+    </React.Fragment>
+    
+  )
+}
+
+export default Home
